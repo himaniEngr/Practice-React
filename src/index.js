c7b6ce9a08b72934eef27d81c135d2b39b722ddc
Himani <himani9.engg@gmail.com>
2022-01-12 17:05:56 +0530

    alternate state initialization

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import ReactDOM from "react-dom";
 // };
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  // constructor(props) {
+  //   super(props);
 
-    this.state = { lat: null, errorMessage: "" };
-    // console.log(this);
-  }
+  //   this.state = { lat: null, errorMessage: "" };
+  //   // console.log(this);
+  // }
+
+  state = { lat: null, errorMessage: "" };
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => this.setState({ lat: position.coords.latitude }),
