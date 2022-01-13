a28ce53f43fcc988994f8d7d7f10805b44bb3ddd
Himani <himani9.engg@gmail.com>
2022-01-13 11:37:59 +0530

    added renderContent method to avoid conditionals in render

--- src/index.js
+++ src/index.js
@@ -25,7 +25,8 @@ class App extends React.Component {
   componentDidUpdate() {
     console.log("My compoenent was rerendered to the screen");
   }
-  render() {
+
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error:{this.state.errorMessage}</div>;
     }
@@ -33,6 +34,10 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
     return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return <div class="border red">{this.renderContent()}</div>;
   }
 }
 
